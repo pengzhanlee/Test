3c97da6a00b1da03e1755bd4036f0b803b7436e6
pengzhanlee <pengzhanlee@pengzhanlee-Latitude-E6220.(none)>
2014-05-19 14:41:32 +0800

    toolbar

--- src/com/jsmix/android/test/cases/toolbar/ToolbarActivity.java
+++ src/com/jsmix/android/test/cases/toolbar/ToolbarActivity.java
@@ -13,7 +13,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.PopupWindow.OnDismissListener;
 import android.widget.TextView;
 
 import com.jsmix.android.test.BaseCasesActivity;
